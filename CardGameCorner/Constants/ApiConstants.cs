90443fce762326167dc3d2cc12005f64575ae6e3
Hemansi Makwana <hemansi.makwana@netwinindia.biz>
2024-11-15 10:30:50 +0530

    Model folder changes

---
+++ CardGameCorner/Constants/ApiConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameCorner.Constants
+{
+    public static class ApiConstants
+    {
+        public static string BaseUrl { get; set; } = "https://www.cardgamecorner.com";
+        public const string ApiBaseUrl = "https://api.magiccorner.it/";
+    }
+}
---
+++ CardGameCorner/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CardGameCorner.Models
+{
+    public class Game
+    {
+        [JsonPropertyName("game")]
+        public string GameCode { get; set; }
+
+        [JsonPropertyName("displayName")]
+        public string DisplayName { get; set; }
+    }
+}
